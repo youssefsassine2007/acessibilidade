567ebfce02651945c8412eac68cd7769b7f78536
youssefsassine2007 <youssef.sassine@escola.pr.gov.br>
2025-02-19 12:38:17 -0300

    19/02/2025

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',function(){
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+let tamanhoAtualFonte = 1;
+aumentaFonteBotao.addEventListener('click',function(){
+tamanhoAtualFonte += 0,1;
+document.body.style.fontSize = 'S(tamanhoAtualFonte)rem';
+
+});
+
+diminuiFonteBotao,addEventListener('click',function(){
+tamanhoAtualFonte += 0,1;
+document.body.style.fontSize = 'S(tamanhoAtualFonte)rem';
+});
+
+});
